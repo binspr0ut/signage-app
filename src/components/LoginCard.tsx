185fcc205a2f54fb2051f855e18695dbb9849ed7
binspr0ut <plattenecho@gmail.com>
2026-03-08 21:02:47 +0700

    Initial Commit

---
+++ src/components/LoginCard.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+
+export default function LoginCard() {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login with your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form action="">
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label>Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <Label>Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="******"
+                required
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <Button variant="outline" className="w-full">
+          Belum punya akun? Register
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
